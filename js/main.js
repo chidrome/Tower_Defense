4382739acc5f12d94c2f8340aa65d1c98674c88e
chidrome <chidrome@gmail.com>
2018-12-10 13:01:37 -0800

    updated the path to make it curve. need to move on for now..probably just better to write a function that keeps take halfway points of two points to make it curve.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var config = {
     type: Phaser.AUTO,
     parent: 'content',
-    width: 640,
-    height: 512,   
+    width: 840,
+    height: 650,   
     scene: {
         key: 'main',
         preload: preload,
@@ -28,12 +28,33 @@ function create() {
     
     // the path for our enemies
     // parameters are the start x and y of our path
-    path = this.add.path(96, -32);
-    path.lineTo(96, 164);
-    path.lineTo(480, 164);
-    path.lineTo(480, 544);
+    path = this.add.path(100, 655);
+    path.lineTo(100, 150); // first turn
+    path.lineTo(107, 130);
+    path.lineTo(125, 117);
+    path.lineTo(150, 112);
+    path.lineTo(225, 100);
+    path.lineTo(275, 95);
+    path.lineTo(300, 93);
+    path.lineTo(350, 90);
+    path.lineTo(425, 93);
+    path.lineTo(475, 100);
+    path.lineTo(550, 125);
+    path.lineTo(600, 150); // second turn
+    path.lineTo(633, 200);
+    path.lineTo(650, 250);
+    path.lineTo(633, 300);
+    path.lineTo(600, 350); // third turn
+    path.lineTo(450, 280);
+    path.lineTo(300, 300); // fourth turn
+    path.lineTo(250, 440);
+    path.lineTo(300, 580); // fifth turn
+    path.lineTo(450, 620);
+    path.lineTo(860, 400); // end
+
+
     
-    graphics.lineStyle(3, 0xffffff, 1);
+    graphics.lineStyle(6, 0xffffff, 1);
     // visualize the path
     path.draw(graphics);
 }
